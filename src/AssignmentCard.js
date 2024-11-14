b1cc84f5366b372f369961ca0363f2b84be0d4a0
ANISHA6 <anisharaj755@gmail.com>
2024-11-14 15:03:25 +0530

    deploy1

--- src/AssignmentCard.js
+++ src/AssignmentCard.js
@@ -1,7 +1,7 @@
 
-import React from 'react';
-// import './style.css';
 
+import React from 'react';
+import './App.css';
 
 const AssignmentCard = () => {
   return (
@@ -15,7 +15,6 @@ const AssignmentCard = () => {
   );
 };
 
-
 const containerStyle = {
   width: '40%',
   maxWidth: '600px',
@@ -27,10 +26,13 @@ const containerStyle = {
   boxShadow: '0 8px 16px rgba(0, 0, 0, 0.5)',
   color: '#ffffff',
   zIndex: 1,
-   
   backgroundImage: 
     'radial-gradient(50% 60% at top center, rgba(18, 230, 200, 0.4), transparent 40%),' + 
     'radial-gradient(50% 80% at  80% 100%, rgba(162, 135, 244, 0.4), transparent 40%)',  
+  '@media (max-width: 768px)': {
+    width: '80%',
+    padding: '20px 30px',
+  },
 };
 
 const gridOverlayStyle = {
@@ -43,7 +45,11 @@ const gridOverlayStyle = {
     'linear-gradient(to right, rgba(255, 255, 255, 0.09) 2px, transparent 1px), linear-gradient(to bottom, rgba(255, 255, 255, 0.09) 2px, transparent 1px)',
   backgroundSize: '40px 40px',
   zIndex: -1,
-  bottom:'40px'
+  bottom: '40px',
+  '@media (max-width: 768px)': {
+    width: '50%',
+    top: '50px',
+  },
 };
 
 const tagStyle = {
@@ -57,22 +63,33 @@ const tagStyle = {
   textTransform: 'uppercase',
   zIndex: 2,
   position: 'relative',
-  borderBottom:"5px gray solid",
-  borderRight:"5px gray solid",
+  borderBottom: '5px gray solid',
+  borderRight: '5px gray solid',
+  '@media (max-width: 768px)': {
+    fontSize: '12px',
+    padding: '6px 20px',
+    margin: '25px 0',
+  },
 };
 
 const titleStyle = {
-  fontSize: '2.5em',
+  fontSize: '2.5em', 
   lineHeight: 1.2,
   marginBottom: '10px',
   zIndex: 2,
   position: 'relative',
   margin: '0',
-  padding: '0', 
-  width:'230%',
+  padding: '0',
+  width: '100%',
   letterSpacing: '0.01em',
+  whiteSpace: 'normal', 
+  wordBreak: 'break-word', 
+  '@media (max-width: 768px)': {
+    fontSize: '1.0em', 
+    width: '100%', 
+    wordBreak: 'break-word', 
+  },
 };
-
 
 
 const companyStyle = {
@@ -82,7 +99,10 @@ const companyStyle = {
   position: 'relative',
   lineHeight: 1.6,
   margin: '30px 0px',
-
+  '@media (max-width: 768px)': {
+    fontSize: '0.8em',
+    margin: '20px 0px',
+  },
 };
 
 export default AssignmentCard;
